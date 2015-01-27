45729e46ca023ae933c75c1662521863a1040172
GlitchEnzo <mccarthp@gmail.com>
2015-01-26 17:23:26 -0800

    Made a Dynamic Script Manager
    Made a dynamic script manager and pulled all of the previous dynamic
    script code out to a separate file.
    
    This is in preparation for allowing the manager to be able to load
    multiple dynamic scripts on the same game object without requiring
    separate monobehaviours for each one.

--- Assets/Dynamic Scripting/DynamicScript.cs
+++ Assets/Dynamic Scripting/DynamicScript.cs
@@ -8,44 +8,40 @@ namespace DynamicScripting
 	using UnityEngine;
 	using System.Collections;
 	using System.IO;
-
+	
 #if TIMING
 	using System.Diagnostics;
 	using Debug = UnityEngine.Debug;
 #endif
 
-	/// <summary>
-	/// Represents a dynamic C# script.
-	/// </summary>
-	public class DynamicScript : MonoBehaviour 
+	public class DynamicScript
 	{
 		/// <summary>
-		/// The script file to be used. (Dragged and dropped in the Unity Editor.)
-		/// Unity supports: .txt, .html, .htm, .xml, .bytes, .json, .csv, .yaml, .fnt
+		/// Gets the full filepath to the C# script file..
 		/// </summary>
-		public TextAsset script;
+		public string Filepath { get; private set; }
 
-		//public string filepath;
+		private string gameObjectName;
 
 		/// <summary>
 		/// The name of the MonoBehaviour contained in the script file.
 		/// </summary>
 		private string behaviourName;
-
+		
 		/// <summary>
 		/// The C# compiler as a service used to compile the script file.
 		/// </summary>
-		private static Evaluator evaluator;
-
+		private Evaluator evaluator;
+		
 		/// <summary>
 		/// The file watcher used to detect when the script file changes.
 		/// </summary>
-	    private FileSystemWatcher watcher;
-
+		private FileSystemWatcher watcher;
+		
 		/// <summary>
 		/// Flag that is marked true when a change in the script file is detected.
-		/// </summary>
-		private bool changed = false;
+        /// </summary>
+        private bool changed = false;
 
 #if TIMING
 		/// <summary>
@@ -54,57 +50,60 @@ namespace DynamicScripting
 		private Stopwatch stopwatch = new Stopwatch();
 #endif
 
-		/// <summary>
-		/// Compiles the script for the first time and sets up the file watcher.
-		/// </summary>
-		void Start() 
+		public DynamicScript(string filepath, string gameObjectName)
 		{
-			// TODO: Get filepath from Test Asset (if possible)
+			Filepath = filepath;
+			this.gameObjectName = gameObjectName;
 
 			// Set up file watcher
-	        watcher = new FileSystemWatcher(Application.streamingAssetsPath, script.name + ".txt");
+			int index = filepath.LastIndexOf("/");
+			watcher = new FileSystemWatcher(filepath.Substring(0, index), filepath.Substring(index + 1));
 			watcher.Changed -= FileChanged;
-	        watcher.Changed += FileChanged;
-	        watcher.EnableRaisingEvents = true;
+			watcher.Changed += FileChanged;
+			watcher.EnableRaisingEvents = true;
 
 			if (evaluator == null)
 			{
 				CompilerContext compilerContext = new CompilerContext(new CompilerSettings(), new UnityReportPrinter());
 				evaluator = new Evaluator(compilerContext);
 				LoadAllAssemblies();
-			}
-
-	        CompileScript();
-		}
+            }
+            
+            CompileScript();
+        }
 
 		/// <summary>
 		/// Compiles the script and attaches it the current GameObject.
 		/// </summary>
-	    void CompileScript()
-	    {
+		private void CompileScript()
+		{
 #if TIMING
 			stopwatch.Reset();
 			stopwatch.Start();
 #endif
 
-			//string code = System.IO.File.ReadAllText(filepath);
-			string code = script.text;
-
-	        // Find the behaviour class name
+			string code = System.IO.File.ReadAllText(Filepath);
+			//string code = script.text;
+			
+			// TODO: Find the namespace name, if there is one
+			
+			// Find the behaviour class name
 			int indexOfClass = code.IndexOf("class") + 5;
 			int indexOfColon = code.IndexOf(":");
 			behaviourName = code.Substring(indexOfClass, indexOfColon - indexOfClass).Trim();
-			//Debug.Log(behaviourName);
-
-	        evaluator.Compile(code);
-	        evaluator.Run("GameObject.Find(\"" + this.name + "\").AddComponent<" + behaviourName + ">();");	
-
-#if TIMING
-			stopwatch.Stop();
-			Debug.Log(string.Format("Compiling took {0} ms", stopwatch.ElapsedMilliseconds));
+#if DEBUG
+			Debug.Log("Class name: " + behaviourName);
 #endif
-	    }
-
+			
+			evaluator.Compile(code);
+			evaluator.Run("GameObject.Find(\"" + gameObjectName + "\").AddComponent<" + behaviourName + ">();");	
+            
+#if TIMING
+            stopwatch.Stop();
+            Debug.Log(string.Format("Compiling took {0} ms", stopwatch.ElapsedMilliseconds));
+#endif
+		}
+        
 		/// <summary>
 		/// Loads all of the currently loaded/referenced assemblies in order to allow the script to access them.
 		/// </summary>
@@ -114,80 +113,79 @@ namespace DynamicScripting
 			stopwatch.Reset();
 			stopwatch.Start();
 #endif
-
+			
 			foreach (System.Reflection.Assembly assembly in System.AppDomain.CurrentDomain.GetAssemblies()) 
 			{
-				//Debug.Log("Loading: " + assembly);
+#if DEBUG
+				Debug.Log("Loading: " + assembly);
+#endif
 				evaluator.ReferenceAssembly(assembly);
 			}
-
+			
 #if TIMING
 			stopwatch.Stop();
 			Debug.Log(string.Format("Loading assemblies took {0} ms", stopwatch.ElapsedMilliseconds));
 #endif
 		}
-
+		
 		/// <summary>
 		/// Called whenever the script file changes.  The event fires on a separate thread from the Unity main thread
 		/// so a flag must be set to recompile the code and reattach the behaviour on the main thread.
 		/// </summary>
 		/// <param name="sender">The sender of the event.</param>
 		/// <param name="e">The event arguments.</param>
-	    void FileChanged(object sender, FileSystemEventArgs e)
-	    {
+		void FileChanged(object sender, FileSystemEventArgs e)
+		{
 #if DEBUG
-	        Debug.Log(string.Format("{0}: {1}", e.ChangeType, e.FullPath));
+			Debug.Log(string.Format("{0}: {1}", e.ChangeType, e.FullPath));
 #endif
 			changed = true;
-	    }
-
+		}
+		
 		/// <summary>
 		/// Recompiles the script if there were any detected changes.
 		/// </summary>
-		void Update() 
+		public void Update() 
 		{
 			if (changed)
 			{
 				changed = false;
-
-				evaluator.Run("GameObject.Destroy(GameObject.Find(\"" + this.name + "\").GetComponent<" + behaviourName + ">());");
+				
+				evaluator.Run("GameObject.Destroy(GameObject.Find(\"" + gameObjectName + "\").GetComponent<" + behaviourName + ">());");
 				CompileScript();
 			}
-
+			
 #if DEBUG
 			// refresh the script
 			if (Input.GetKeyDown(KeyCode.F5))
 			{
-				evaluator.Run("GameObject.Destroy(GameObject.Find(\"" + this.name + "\").GetComponent<" + behaviourName + ">());");
+				evaluator.Run("GameObject.Destroy(GameObject.Find(\"" + gameObjectName + "\").GetComponent<" + behaviourName + ">());");
 				CompileScript();
 			}
-
+			
 			// remove the script
-			if (Input.GetKeyDown(KeyCode.Delete))
-			{
-			    Component scriptComponent = gameObject.GetComponent(behaviourName);
-				Debug.Log(scriptComponent == null);
-	            evaluator.Run("GameObject.Destroy(GameObject.Find(\"" + this.name + "\").GetComponent<" + behaviourName + ">());");
-			}
-
-			// add the script
-	        if (Input.GetKeyDown(KeyCode.Insert))
-	        {
-	            CompileScript();
-	        }
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+				evaluator.Run("GameObject.Destroy(GameObject.Find(\"" + gameObjectName + "\").GetComponent<" + behaviourName + ">());");
+            }
+            
+            // add the script
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                CompileScript();
+            }
 #endif
-		}
-
-		/// <summary>
-		/// Unsubscribes the file watcher when the component is destroyed.
-		/// </summary>
-		void OnDestroy()
-		{
-			if (watcher != null)
-			{
-				// unsubscribe the file watcher
-				watcher.Changed -= FileChanged;
-			}
-		}
+        }
+        
+        /// <summary>
+        /// Unsubscribes the file watcher.
+        /// </summary>
+        public void StopWatching()
+        {
+            if (watcher != null)
+            {
+                watcher.Changed -= FileChanged;
+            }
+        }
 	}
 }
---
+++ Assets/Dynamic Scripting/DynamicScriptManager.cs
@@ -0,0 +1,55 @@
+﻿namespace DynamicScripting
+{
+	using System;
+	using Mono.CSharp;
+	using UnityEngine;
+	using System.Collections;
+	using System.IO;
+
+	/// <summary>
+	/// Represents a dynamic C# script.
+	/// </summary>
+	public class DynamicScriptManager : MonoBehaviour 
+	{
+		/// <summary>
+		/// The script file to be used. (Dragged and dropped in the Unity Editor.)
+		/// Unity supports: .txt, .html, .htm, .xml, .bytes, .json, .csv, .yaml, .fnt
+		/// </summary>
+		public TextAsset scriptAsset;
+
+		//public string filepath;
+
+		DynamicScript script;
+
+		/// <summary>
+		/// Compiles the script for the first time and sets up the file watcher.
+		/// </summary>
+		void Start() 
+		{
+			// TODO: Get filepath from Test Asset (if possible)
+			script = new DynamicScript(Application.streamingAssetsPath + "/" + scriptAsset.name + ".txt", this.name);
+		}
+
+		/// <summary>
+		/// Recompiles the script if there were any detected changes.
+		/// </summary>
+		void Update() 
+		{
+			if (script != null) 
+			{
+				script.Update ();
+			}
+		}
+
+		/// <summary>
+		/// Unsubscribes the file watcher when the component is destroyed.
+		/// </summary>
+		void OnDestroy()
+		{
+			if (script != null) 
+			{
+				script.StopWatching ();
+			}
+		}
+	}
+}
